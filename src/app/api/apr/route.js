6d46b1ab37a42b0979bea72400c448a5763f3947
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-09-14 14:17:32 +0530

    api folder added and top pools

---
+++ src/app/api/apr/route.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await axios.get("http://localhost:3000/api/db");
+  const poolData = res.data.PoolData;
+  const flattenedPoolData = [].concat(...poolData); 
+  const sortedPoolData = flattenedPoolData
+    .slice()
+    .sort((a, b) => b.apy - a.apy);
+
+  const APR = [];
+  for (const pool of sortedPoolData) {
+    let data;
+
+    data = {
+      slug: pool.project,
+      apy: pool.apy,
+      symbol: pool.symbol,
+    };
+    APR.push(data);
+  }
+
+  return NextResponse.json(APR)
+};
