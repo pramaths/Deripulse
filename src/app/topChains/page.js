728c795ac26b97b7767b98ef14ba8cbcc7d7fe72
Noveleader <ankush@DESKTOP-J64JB2V>
2023-09-20 12:06:25 +0530

    topChains update

---
+++ src/app/topChains/page.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import TopChains from "../components/TopChains";
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <TopChains />
+      </div>
+    </div>
+  );
+}
