554f4bb1407eab0b7b2fb7cddc9c8da5abc82382
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-09-21 11:11:43 +0530

    commented some files debugging ...

--- src/app/api/[chains]/route.js
+++ src/app/api/[chains]/route.js
@@ -1,36 +1,34 @@
-import axios from "axios";
-import TVLChart from "../chartNetTVL";
-import { NextResponse } from "next/server";
+// import axios from "axios";
+// import TVLChart from "../chartNetTVL";
+// import { NextResponse } from "next/server";
 
+// export async function GET(request,{ params }) {
+//   const chain = params.chains;
+//   try {
+//     const backendResponse = await axios.get("http://localhost:3000/api/db");
+//     const protocolData = backendResponse.data.arrProtocolData;
+//     const poolData = backendResponse.data.PoolData;
+//     const properChainName = chain.charAt(0).toUpperCase() + chain.slice(1);
+//     const filteredProtocols = protocolData.filter((protocol) =>
+//       protocol.chains.includes(properChainName)
+//     );
 
+//     const responseData = [];
 
-export async function GET(request,{ params }) {
-  const chain = params.chains;
-  try {
-    const backendResponse = await axios.get("http://localhost:3000/api/db");
-    const protocolData = backendResponse.data.arrProtocolData;
-    const poolData = backendResponse.data.PoolData;
-    const properChainName = chain.charAt(0).toUpperCase() + chain.slice(1);
-    const filteredProtocols = protocolData.filter((protocol) =>
-      protocol.chains.includes(properChainName)
-    );
+//     for (let i = 0; i < filteredProtocols.length; i++) {
+//       if (filteredProtocols[i].poolLength > 0) {
+//         filteredProtocols[i].pool = poolData[filteredProtocols[i].poolindex];
+//       }
+//       responseData.push(filteredProtocols[i]);
+//     }
 
-    const responseData = [];
+//     const chartData = await TVLChart(responseData);
 
-    for (let i = 0; i < filteredProtocols.length; i++) {
-      if (filteredProtocols[i].poolLength > 0) {
-        filteredProtocols[i].pool = poolData[filteredProtocols[i].poolindex];
-      }
-      responseData.push(filteredProtocols[i]);
-    }
+//     responseData.push(chartData);
 
-    const chartData = await TVLChart(responseData);
-
-    responseData.push(chartData);
-
-    return NextResponse.json(responseData); 
-  } catch (error) {
-    console.error("An error occurred:", error);
-    return NextResponse.json({ error: error.message }); 
-  }
-};
+//     return NextResponse.json(responseData); 
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//     return NextResponse.json({ error: error.message }); 
+//   }
+// };
--- src/app/api/apr/route.js
+++ src/app/api/apr/route.js
@@ -1,39 +1,39 @@
-const axios = require("axios");
-import { NextResponse } from "next/server";
-let res;
+// const axios = require("axios");
+// import { NextResponse } from "next/server";
+// let res;
 
-async function logo(slug) {
-  const protocolData = res.data.arrProtocolData;
-  const resp = protocolData.filter((prot) => prot.slug === slug);
-  return resp[0].logo;
-}
-export async function GET() {
-  res = await axios.get("http://localhost:3000/api/db");
+// async function logo(slug) {
+//   const protocolData = res.data.arrProtocolData;
+//   const resp = protocolData.filter((prot) => prot.slug === slug);
+//   return resp[0].logo;
+// }
+// export async function GET() {
+//   res = await axios.get("http://localhost:3000/api/db");
 
-  const poolData = res.data.PoolData;
-  const flattenedPoolData = [].concat(...poolData);
-  const sortedPoolData = flattenedPoolData
-    .slice()
-    .sort((a, b) => b.apyPct1D - a.apyPct1D);
+//   const poolData = res.data.PoolData;
+//   const flattenedPoolData = [].concat(...poolData);
+//   const sortedPoolData = flattenedPoolData
+//     .slice()
+//     .sort((a, b) => b.apyPct1D - a.apyPct1D);
 
-  const APR = [];
-  for (const pool of sortedPoolData) {
-    let data;
-    data = {
-      slug: pool.project,
-      apy1D: pool.apyPct1D,
-      symbol: pool.symbol,
-      tvlUSD: pool.tvlUsd,
-      apy7D: pool.apyPct7D,
-      apy30D: pool.apyPct30D,
-      apy: pool.apy,
-      prediction: pool.predictions,
-      logo: await logo(pool.project),
-      poolMeta: pool.poolMeta,
-      chain: pool.chain,
-    };
-    APR.push(data);
-  }
+//   const APR = [];
+//   for (const pool of sortedPoolData) {
+//     let data;
+//     data = {
+//       slug: pool.project,
+//       apy1D: pool.apyPct1D,
+//       symbol: pool.symbol,
+//       tvlUSD: pool.tvlUsd,
+//       apy7D: pool.apyPct7D,
+//       apy30D: pool.apyPct30D,
+//       apy: pool.apy,
+//       prediction: pool.predictions,
+//       logo: await logo(pool.project),
+//       poolMeta: pool.poolMeta,
+//       chain: pool.chain,
+//     };
+//     APR.push(data);
+//   }
 
-  return NextResponse.json(APR);
-}
+//   return NextResponse.json(APR);
+// }
