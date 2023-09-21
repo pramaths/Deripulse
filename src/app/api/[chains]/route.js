49d84beea8facc907a707ea14ba85364f6a562cb
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-09-21 11:45:56 +0530

    used /api/db

--- src/app/api/[chains]/route.js
+++ src/app/api/[chains]/route.js
@@ -1,34 +1,34 @@
-// import axios from "axios";
-// import TVLChart from "../chartNetTVL";
-// import { NextResponse } from "next/server";
+import axios from "axios";
+import TVLChart from "../chartNetTVL";
+import { NextResponse } from "next/server";
 
-// export async function GET(request,{ params }) {
-//   const chain = params.chains;
-//   try {
-//     const backendResponse = await axios.get("http://localhost:3000/api/db");
-//     const protocolData = backendResponse.data.arrProtocolData;
-//     const poolData = backendResponse.data.PoolData;
-//     const properChainName = chain.charAt(0).toUpperCase() + chain.slice(1);
-//     const filteredProtocols = protocolData.filter((protocol) =>
-//       protocol.chains.includes(properChainName)
-//     );
+export async function GET(request,{ params }) {
+  const chain = params.chains;
+  try {
+    const backendResponse = await axios.get(`/api/db/`);
+    const protocolData = backendResponse.data.arrProtocolData;
+    const poolData = backendResponse.data.PoolData;
+    const properChainName = chain.charAt(0).toUpperCase() + chain.slice(1);
+    const filteredProtocols = protocolData.filter((protocol) =>
+      protocol.chains.includes(properChainName)
+    );
 
-//     const responseData = [];
+    const responseData = [];
 
-//     for (let i = 0; i < filteredProtocols.length; i++) {
-//       if (filteredProtocols[i].poolLength > 0) {
-//         filteredProtocols[i].pool = poolData[filteredProtocols[i].poolindex];
-//       }
-//       responseData.push(filteredProtocols[i]);
-//     }
+    for (let i = 0; i < filteredProtocols.length; i++) {
+      if (filteredProtocols[i].poolLength > 0) {
+        filteredProtocols[i].pool = poolData[filteredProtocols[i].poolindex];
+      }
+      responseData.push(filteredProtocols[i]);
+    }
 
-//     const chartData = await TVLChart(responseData);
+    const chartData = await TVLChart(responseData);
 
-//     responseData.push(chartData);
+    responseData.push(chartData);
 
-//     return NextResponse.json(responseData); 
-//   } catch (error) {
-//     console.error("An error occurred:", error);
-//     return NextResponse.json({ error: error.message }); 
-//   }
-// };
+    return NextResponse.json(responseData); 
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return NextResponse.json({ error: error.message }); 
+  }
+};
